75f4222824bc57985093dea97cb4868c6cbe92d1
Caroline Eubanks <cmeubanks@gmail.com>
2021-02-03 08:21:55 -0700

    completed dragon test suite

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,22 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.eatCounter = 0;
+  }
+  greet() {
+    return `Hi, ${this.rider}!`;
+  }
+  eat() {
+    this.eatCounter++
+    if(this.eatCounter > 2) {
+      return this.hungry = false;
+    } else{
+      return this.hungry = true;
+    }
+  }
+}
 
+
+module.exports = Dragon;
