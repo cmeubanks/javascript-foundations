98da050d0c6c97a92fe52282b81fc2fdcaa6c80c
Caroline Eubanks <cmeubanks@gmail.com>
2021-02-03 07:59:05 -0700

    completed unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -2,6 +2,16 @@ class Unicorn {
   constructor(name, color) {
     this.name = name;
     this.color = color;
+
+    if(color === undefined) {
+      return this.color = 'white';
+    };
+  }
+  isWhite() {
+    return false;
+  }
+  says(quote) {
+    return `**;* ${quote} *;**`;
   }
 }
 
