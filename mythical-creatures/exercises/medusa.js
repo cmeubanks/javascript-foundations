d8bda284e7432e6bb4c4cdfca8f00f0f03c4e5b6
Caroline Eubanks <cmeubanks@gmail.com>
2021-02-16 07:09:08 -0700

    completed medusa

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,23 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+  this.name = name;
+  this.statues = [];
+  }
+  gazeAtVictim(person) {
+    var victim = new Statue(person.name)
+    if(this.statues.length <= 2){
+    this.statues.push(victim)
+  } else {
+    this.statues.push(victim)
+    var returnPerson = new Person(this.statues[0].name)
+    returnPerson.mood = 'relieved';
+    this.statues.shift();
+    return returnPerson
+  }
+  }
+};
+
+module.exports = Medusa;
