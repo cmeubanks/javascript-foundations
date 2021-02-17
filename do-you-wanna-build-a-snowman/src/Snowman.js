7db3a818aff6a6bd5abe908bebc79575d62e8f4d
Caroline Eubanks <cmeubanks@gmail.com>
2021-02-16 17:40:47 -0700

    completed Werewolf

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,20 @@
 class Snowman {
-
+constructor(snowmanDetails) {
+  this.carrots = snowmanDetails.carrots;
+  this.coal = snowmanDetails.coal;
+  this.buttons = snowmanDetails.buttons;
+  this.snowballs = snowmanDetails.snowballs;
+  this.magicHat = false;
+  }
+  canWearMagicHat() {
+  //if coal eyes <2, magic hat will be false
+  if (this.coal < 2 || this.buttons < 5) {
+  this.magicHat = false;
+  } else {
+  this.magicHat = true;
+  }
+  }
 }
+
 
 module.exports = Snowman;
