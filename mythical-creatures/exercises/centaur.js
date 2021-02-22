a877ccdcd5315371e47fcdf83814a89c1bbc1772
Caroline Eubanks <cmeubanks@gmail.com>
2021-02-21 21:16:42 -0700

    completed centaur test

--- mythical-creatures/exercises/centaur.js
+++ mythical-creatures/exercises/centaur.js
@@ -1,6 +1,54 @@
 class Centaur {
-  constructor() {
-
+  constructor(object) {
+    this.name = object.name;
+    this.breed = object.type;
+    this.cranky = false;
+    this.standing = true;
+    this.layingDown = false;
+    this.counter = 0;
+  }
+  shootBow() {
+    this.counter+=1
+    if(this.counter === 3 || this.cranky){
+      this.cranky = true;
+      return 'NO!'
+    }
+    if(!this.standing && this.layingDown) {
+      return 'NO!'
+    }
+    return 'Twang!!!'
+  }
+  run() {
+    this.counter+=1
+    if(this.counter === 3 || this.cranky){
+      this.cranky = true;
+      return 'NO!'
+    }
+    if(!this.standing && this.layingDown) {
+      return 'NO!'
+    }
+    return 'Clop clop clop clop!!!'
+  }
+  sleep() {
+    if(!this.standing && this.layingDown) {
+      this.cranky = false;
+      return 'ZZZZ'
+    }
+    return 'NO!'
+  }
+  layDown() {
+    this.standing = false;
+    this.layingDown = true;
+  }
+  standUp() {
+    this.standing = true;
+    this.layingDown = false;
+  }
+  drinkPotion() {
+    this.cranky = false;
+    if(!this.standing && this.layingDown) {
+      return 'Not while I\'m laying down!'
+    }
   }
 };
 
