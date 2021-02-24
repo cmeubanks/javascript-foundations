81e13c2357ee217b6db99b687d4b1c8889b17b58
Caroline Eubanks <cmeubanks@gmail.com>
2021-02-24 09:08:54 -0700

    part of fairy

--- mythical-creatures/exercises/fairy.js
+++ mythical-creatures/exercises/fairy.js
@@ -2,9 +2,28 @@ class Fairy {
   constructor(name) {
     this.name = name;
     this.dust = 10;
+    this.disposition = 'Good natured';
+    this.clothes = {
+      dresses: ['Iris'],
+    };
   }
   receiveBelief(){
     this.dust = 11;
+  }
+  believe() {
+    this.dust = 20;
+  }
+  makeDresses(array){
+    for(var i = 0; i < array.length; i++){
+    this.clothes.dresses.push(array[i]);
+  }
+  }
+  becomeProvoked() {
+    this.disposition = 'Vengeful';
+  }
+  replaceInfant(object) {
+    this.disposition = 'Malicious';
+    object.disposition = this.disposition;
   }
 }
 
