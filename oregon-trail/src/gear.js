bc64db56034a20270d4c1b23f97cadcca7be5278
Caroline Eubanks <cmeubanks@gmail.com>
2021-03-01 13:51:05 -0700

    completed gear & part

--- oregon-trail/src/gear.js
+++ oregon-trail/src/gear.js
@@ -1,5 +1,15 @@
 class Gear {
-
+  constructor(type, num) {
+  this.type = type;
+  this.quantity = num;
+  }
+  checkForValidity() {
+    if (this.type === ('ammunition' || 'food' || 'clothes')) {
+    return `Great, we\'ll need lots of ${this.type}!`
+  } else {
+    return `I don\`t think a ${this.type} will help us.`
+  }
+  }
 }
 
 module.exports = Gear;
