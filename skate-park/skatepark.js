2af8426ab3d21e9e5e4c82164b81381ff5cf3d3e
Caroline Eubanks <cmeubanks@gmail.com>
2021-03-03 08:49:06 -0700

    completed skatepark

--- skate-park/skatepark.js
+++ skate-park/skatepark.js
@@ -8,6 +8,21 @@ class SkatePark {
     this.cost = skatePark.price || 0;
     this.occupants = [];
   }
+  admit(skater) {
+    if(this.isPrivate && skater.money >= this.cost && this.occupants.length < 3){
+      var moneyLeft = skater.money - this.cost
+      skater.money = moneyLeft
+      this.occupants.push(skater)
+      return `Welcome to ${this.name}, the cost will be $${this.cost}.00.`
+    } else if(!this.isPrivate){
+    this.occupants.push(skater)
+    return `Welcome to the free ${this.name} Skatepark!`
+  } else if(skater.money < this.cost){
+    return `Sorry, you don't have enough money.`;
+  } else {
+    return 'Sorry, we are at max capacity. Thank you for understanding.'
+  }
+  }
 };
 
 module.exports = SkatePark;
