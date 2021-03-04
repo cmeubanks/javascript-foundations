0a90a92cf9cbb539d1c7d2dc5a968cefac823aa3
Caroline Eubanks <cmeubanks@gmail.com>
2021-03-04 06:46:47 -0700

    completed lunchbox

--- golf-course/golf-course.js
+++ golf-course/golf-course.js
@@ -1,6 +1,22 @@
 class GolfCourse {
-
-
+constructor(name, difficulty, openings, features) {
+    this.name = name;
+    this.difficulty = difficulty;
+    this.openings = openings;
+    this.features = features;
+    this.currentlyPlaying = [];
 }
 
+checkInGroup(groupOfGolfers){
+  if(this.openings >= groupOfGolfers.length) {
+    this.openings = this.openings - groupOfGolfers.length
+      for (var i = 0; i < groupOfGolfers.length; i++){
+        this.currentlyPlaying.unshift(groupOfGolfers[i].name);
+      }
+      return `You're checked in. Have fun!`
+    } else {
+      return 'Sorry, we are currently booked! Please come back later.'
+    }
+  }
+}
 module.exports = GolfCourse;
--- golf-course/golfer.js
+++ golf-course/golfer.js
@@ -1,6 +1,41 @@
+const GolfCourse = require("./golf-course");
+
 class Golfer {
-
-
+constructor(golfer) {
+    this.name = golfer.name;
+    this.handicap = golfer.handicap;
+    this.frustration = 0;
+    this.confidence = 0;
 }
-
+calculateAvg(par) {
+ var parResult = this.handicap + par;
+ return `I usually shoot a ${parResult} on average.`
+}
+playRound(golfCourse) {
+ if(golfCourse.difficulty === 'hard'){
+     this.frustration+=500
+ }
+ if(golfCourse.difficulty === 'moderate'){
+     this.frustration+=100
+ }
+}
+hitTheRange() {
+  this.confidence+=10
+}
+marvel(golfCourse) {
+  return `I love the ${golfCourse.features[0]} and ${golfCourse.features[1]} on this course!`
+}
+whatYaShoot(num) {
+ if(num < 0){
+   this.frustration = 0;
+   return 'I AM THE GREATEST GOLFER ALIVE!'
+ } else if(num === 0){
+     this.frustration-=10
+     return 'Booyah!'
+ } else {
+     this.frustration+=20
+     return 'Doh!'
+ }
+}
+}
 module.exports = Golfer;
