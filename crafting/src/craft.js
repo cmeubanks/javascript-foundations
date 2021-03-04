2c19a79fea355165e9a10fe5fcc4e7f538b1d974
Caroline Eubanks <cmeubanks@gmail.com>
2021-03-04 09:38:22 -0700

    completed crafting

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,20 @@
 class Craft {
+  constructor(craft) {
+    this.name = craft.type;
+    this.materials = craft.materials;
+    this.completed = false;
+  }
 
+  completeCraft() {
+    this.completed = true;
+    return 'All done! It looks great!';
+  }
+
+  calculateProjectTotal() {
+    var cost1 = this.materials[0].calculateMaterialCost()
+    var cost2 = this.materials[1].calculateMaterialCost()
+    return cost1 + cost2
+  }
 }
 
 module.exports = Craft;
